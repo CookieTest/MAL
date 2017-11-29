739cf3eee29c8134a1791c9a215671e38b56773b
CookieTest <keksikoe1234@gmail.com>
2017-11-29 23:40:48 +0200

    Update va-seen-anime-find-roles.js

--- va-seen-anime-find-roles.js
+++ va-seen-anime-find-roles.js
@@ -1,2 +1,11 @@
+//Shit is WIP, it doesnt' work if you don't have 18if on your list (status doesn't matter)
+//
+
 var xresult = document.evaluate("//tbody[contains(., '18if')]", document, null, XPathResult.ANY_TYPE, null);
 var nextxresult = xresult.iterateNext();
+
+
+/* TODO
+* Output the relevant anime.
+* Change how correct table is selected to something sensible.
+ */
