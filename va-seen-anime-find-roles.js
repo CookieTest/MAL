e37364272d6939f94ee9b220503de403ae2497a8
CookieTest <keksikoe1234@gmail.com>
2017-11-29 23:42:44 +0200

    Update va-seen-anime-find-roles.js
    Tidy up extra lines.

--- va-seen-anime-find-roles.js
+++ va-seen-anime-find-roles.js
@@ -1,5 +1,4 @@
 //Shit is WIP, it doesnt' work if you don't have 18if on your list (status doesn't matter)
-//
 
 var xresult = document.evaluate("//tbody[contains(., '18if')]", document, null, XPathResult.ANY_TYPE, null);
 var nextxresult = xresult.iterateNext();
